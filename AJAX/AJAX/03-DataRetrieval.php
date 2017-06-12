21fd1141220f874bc9e58b488f496827715dc196
ravi4all <ravikanttyagi.2295@gmail.com>
2017-06-12 11:07:39 +0530

    Add files via upload

---
+++ AJAX/AJAX/03-DataRetrieval.php
@@ -0,0 +1,3 @@
+<?php
+echo "<h3> Welcome $_POST["fname"] $_POST["lname"] </h3>" ?>
+?>
---
+++ AJAX/AJAX/AsynchCall.js
@@ -0,0 +1,29 @@
+window.addEventListener("load",init);
+
+function init(){
+    document.getElementById("btn").addEventListener("click",loadData);
+}
+
+function loadData() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("result").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "ajax_data.txt", true);
+  xhttp.send();
+}
+
+/*
+readyState	Holds the status of the XMLHttpRequest.
+0: request not initialized 
+1: server connection established
+2: request received 
+3: processing request 
+4: request finished and response is ready
+
+status	Returns the status-number of a request
+200: "OK"
+403: "Forbidden"
+404: "Not Found"*/
